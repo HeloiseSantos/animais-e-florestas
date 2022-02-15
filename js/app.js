c09150eee67d1a57f3565096dd15b1d0fbde58cf
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-02-14 21:38:33 -0300

    Adiciona classe active ao clicar no botão para ativar itens

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-$('.tab-menu a').click(function() {
-    
+$(document).ready(function() {
+    $('[data-group]').each(function() {
+        var $allTarget = $(this).find('[data-target]'),
+            $allClick = $(this).find('[data-click]'),
+            activeClass = 'active';
+
+        $allTarget.first().addClass(activeClass);
+        $allClick.first().addClass(activeClass);
+
+        $allClick.click(function(e) {
+            e.preventDefault();
+
+            var idData = $(this).data('click'),
+                $target = $('[data-target="' + idData + '"]');
+
+            $allClick.removeClass(activeClass);
+            $allTarget.removeClass(activeClass);
+
+            $target.addClass(activeClass);
+            $(this).addClass(activeClass);
+
+        });
+    });
 });
