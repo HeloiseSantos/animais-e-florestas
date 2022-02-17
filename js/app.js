a943ae8e98942be246118ce5a1dc181446a8d104
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-02-16 22:02:47 -0300

    Configura funcionamento do menu mobile

--- js/app.js
+++ js/app.js
@@ -59,4 +59,9 @@ $(document).ready(function() {
             }
         });
     });
+
+    $('.mobile-btn').click(function() {
+        $(this).toggleClass('active');
+        $('.mobile-menu').toggleClass('active');
+    });
 });
